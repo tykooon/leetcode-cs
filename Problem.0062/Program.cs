c7d3d74d018554282b647e73dc3e795ecab051c6
Alex Tykoun <alextykoun@gmail.com>
2024-06-02 01:34:16 +0300

    Task 62 added

---
+++ Problem.0062/Program.cs
@@ -0,0 +1,14 @@
+﻿//      https://leetcode.com/problems/unique-paths/
+
+Console.WriteLine(UniquePaths(8, 8));
+
+int UniquePaths(int m, int n)
+{
+    long res = 1;
+    for (var i = 0; i < n - 1; i++)
+    {
+        res *= m + i;
+        res /= (i + 1);
+    }
+    return (int)res;
+}
