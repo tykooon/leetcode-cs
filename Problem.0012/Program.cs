c0eb4b810352c2a81947de98f0f6adc9e0c40692
Alex Tykoun <alextykoun@gmail.com>
2024-06-03 21:21:17 +0300

    Task 12 and Task 13 solved

---
+++ Problem.0012/Program.cs
@@ -0,0 +1,48 @@
+﻿//    https://leetcode.com/problems/integer-to-roman/
+
+Console.WriteLine(IntToRoman(3749));
+
+string IntToRoman(int num)
+{
+    var thousands = num / 1000;
+    var hundreds = (num - thousands * 1000) / 100;
+    var tens = (num - thousands * 1000 - hundreds * 100) / 10;
+    var units = num % 10;
+
+    var result = new StringBuilder();
+    AddSomeDigits(result, "M", thousands);
+
+    AddOrder(result, hundreds, "C", "D", "M");
+    AddOrder(result, tens, "X", "L", "C");
+    AddOrder(result, units, "I", "V", "X");
+
+    return result.ToString();
+}
+
+void AddOrder(StringBuilder result, int nominal, string one, string five, string ten)
+{
+    switch (nominal)
+    {
+        case < 4:
+            AddSomeDigits(result, one, nominal);
+            break;
+        case 4:
+            AddSomeDigits(result, one + five, 1);
+            break;
+        case > 4 and < 9:
+            AddSomeDigits(result, five, 1);
+            AddSomeDigits(result, one, nominal - 5);
+            break;
+        case 9:
+            AddSomeDigits(result, one + ten, 1);
+            break;
+    }
+}
+
+void AddSomeDigits(StringBuilder result, string digit, int times)
+{
+    for (var i = 0; i < times; i++)
+    {
+        result.Append(digit);
+    }
+}
---
+++ Problem.0013/Program.cs
@@ -0,0 +1,22 @@
+﻿//      https://leetcode.com/problems/roman-to-integer/
+
+Console.WriteLine(RomanToInt("MMCMXCIX"));
+
+int RomanToInt(string s)
+{
+    string[] digits = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+    int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    int sPointer = 0;
+    int result = 0;
+
+    for (var i = 0; i < values.Length; i++)
+    {
+        while (s[sPointer..].StartsWith(digits[i]))
+        {
+            sPointer += (i % 2) + 1;
+            result += values[i];
+        }
+    }
+
+    return result;
+}
