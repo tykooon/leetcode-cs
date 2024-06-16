7b84380c03a68c326e6cdb066284635dc775f115
Alex Tykoun <alextykoun@gmail.com>
2024-06-16 21:32:40 +0300

    Task 15 & 16 solved

--- Problem.0015/Program.cs
+++ Problem.0015/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//      https://leetcode.com/problems/3sum/
+
+var res = ThreeSum(new int[] { -4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6 });
+Console.WriteLine(res.Any());
+
+foreach (var item in res)
+{
+    Console.WriteLine($"[{item[0]}, {item[1]}, {item[2]}]");
+}
+
+
+IList<IList<int>> ThreeSum(int[] nums)
+{
+    var res = new List<IList<int>>();
+    if (nums.Length < 3)
+    {
+        return res;
+    }
+
+
+    var ordered = nums.Order().ToList();
+    var len = ordered.Count();
+    var pFirst = 0;
+    var pSecond = 1;
+    var pThird = len - 1;
+
+    while (pSecond < pThird && ordered[pFirst] <= 0)
+    {
+        var sum = ordered[pFirst] + ordered[pSecond] + ordered[pThird];
+        if (sum == 0 && (!res.Any() || res[^1][0] != ordered[pFirst] || res[^1][1] != ordered[pSecond]))
+        {
+            res.Add(new int[] { ordered[pFirst], ordered[pSecond], ordered[pThird] });
+        }
+
+        (pSecond, pThird) = sum < 0 ? (pSecond + 1, pThird) : (pSecond, pThird - 1);
+        if (pSecond < pThird)
+        {
+            continue;
+        }
+
+        if (pSecond >= pThird)
+        {
+            do
+            {
+                pFirst++;
+            }
+            while (pFirst < len && ordered[pFirst] == ordered[pFirst - 1]);
+
+            pSecond = pFirst + 1;
+            pThird = len - 1;
+        }
+    }
+
+    return res;
+}
--- Problem.0016/Program.cs
+++ Problem.0016/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//      https://leetcode.com/problems/3sum-closest/
+
+
+var res = ThreeSumClosest(new int[] { -4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6 }, 100);
+Console.WriteLine($"{res}");
+
+
+int ThreeSumClosest(int[] nums, int target)
+{
+    var ordered = nums.Order().ToArray();
+    var len = ordered.Count();
+    var pFirst = 0;
+    var pSecond = 1;
+    var pThird = len - 1;
+    var res = ordered[pFirst] + ordered[pSecond] + ordered[pThird];
+
+    while (pSecond < pThird)
+    {
+        var sum = ordered[pFirst] + ordered[pSecond] + ordered[pThird];
+        if (sum == target)
+        {
+            return sum;
+        }
+
+        if (Math.Abs(sum - target) < Math.Abs(res - target))
+        {
+            res = sum;
+        }
+
+        (pSecond, pThird) = sum < target ? (pSecond + 1, pThird) : (pSecond, pThird - 1);
+        if (pSecond < pThird)
+        {
+            continue;
+        }
+
+        if (pSecond >= pThird)
+        {
+            pFirst++;
+            pSecond = pFirst + 1;
+            pThird = len - 1;
+        }
+    }
+
+    return res;
+}
